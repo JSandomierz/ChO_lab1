a037c752016e05e8e68ba12c09c4e53e83b90d4f
Krzysztof Kleba <krzysztof.kleba@nokia.com>
2020-04-15 19:48:12 +0200

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package ChO_lab1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Jre version:" + System.getProperty("java.runtime.version"));
+        System.out.println("Jdk version:" + System.getProperty("java.version"));
+    }
+}
