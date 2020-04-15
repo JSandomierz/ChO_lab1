99afa0eecf9d7cbc1d79d56bb549f33c4e3735bc
Jakub Sandomierz <jakubsandomierz@gmail.com>
2020-04-15 20:09:28 +0200

    Project name added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,12 @@
 package ChO_lab1.src;
 
 public class HelloWorld {
+    
+    public static final String projectName = "ChO_lab1";
+    
     public static void main(String[] args) {
         System.out.println("Jre version:" + System.getProperty("java.runtime.version"));
         System.out.println("Jdk version:" + System.getProperty("java.version"));
+        System.out.println("Project name: " + projectName);
     }
 }
